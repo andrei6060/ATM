c1e0bf8da87471cfc87315943a038dd89c6ca91a
Constantinescu Andrei <andrei.iunie2002@gmail.com>
2023-07-19 00:03:39 +0300

    creating the setters and getters and the main method

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class cardHolder
+{
+    String cardNum;
+    int cardPin;
+    String firstName;
+    String lastName;
+    double balance;
+
+    public cardHolder(string cardNum, int cardPin, string firstName, string lastName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.cardPin = cardPin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+    public String getCardNum() { return cardNum; }
+    public void setCardNum(String cardNum) {  this.cardNum = cardNum; }
+
+
+    public int getCardPin() { return cardPin; }
+    public void setCardPin(int cardPin) { this.cardPin = cardPin; }    
+
+
+    public String getFirstName() { return firstName; } 
+    public void setFirstName(String firstName) { this.firstName = firstName;}
+
+
+    public String getLastName() { return lastName;}
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    
+
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) {  this.balance = balance; }
+
+    public static void main(String[] args)
+    {
+        void printOpptions()
+        {
+            Console.WriteLine("Choose one of the options below:");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+         }
+
+        void deposit(cardHolder cardHolder)
+        {
+
+        }
+    }
+
+}
